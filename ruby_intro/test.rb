8df5b669d0507570646f8b2929125c2ae96a4513
priyanka19697 <priyanka19697@gmail.com>
2019-10-18 15:42:09 +0530

    rvm

--- ruby_intro/test.rb
+++ ruby_intro/test.rb
@@ -41,14 +41,63 @@
 # 4 main ways to call a proc
 
 
-my_proc = Proc.new do |x|
-    puts "This is my proc and #{x} was passed to me"
-end
+# my_proc = Proc.new do |x|
+#     puts "This is my proc and #{x} was passed to me"
+# end
 
-my_proc.call(10)
+# my_proc.call(10)
 
-my_proc.(20)
+# my_proc.(20) implicit call
 
-my_proc[50]
+# my_proc[50]
 
-my_proc === 40
+# my_proc === 40 case equality operator
+
+# lambdas are very simila
+
+# class Computer
+#     @@users = Hash.new
+#     attr_reader:files
+#     def initialize(username, password)
+#       @username = username
+#       @password = password
+#       @files = Hash.new
+#       @@users[username] = password
+#     end
+    
+#     def create(filename) 
+#       @files[filename] = Time.now
+#       puts "new file created"
+#     end
+
+#     def delete(filename)
+#         @files.delete(filename)
+#         puts "file deleted"
+#     end
+    
+#     def self.get_users
+#       @@users
+#     end
+#   end
+  
+#   my_computer = Computer.new("priyanka", 123456)
+
+#   aks_computer = Computer.new("ak",13456)
+#   puts Computer.get_users
+
+#   my_computer.create("file1")
+#   aks_computer.create("file2") 
+#   aks_computer.create("file1")
+
+#   puts aks_computer.files
+#   aks_computer.delete("file1")
+
+#   puts aks_computer.files
+
+#   self is like this
+
+
+#rvm ruby version manager rvm use 1.9.2 makes ruby version 1.9.2
+
+
+
